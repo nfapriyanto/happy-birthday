33d22c1ec9d562aa0f18b663430db0c177f7769a
nfapriyanto <109157861+nfapriyanto@users.noreply.github.com>
2024-08-06 00:17:11 +0700

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Aug 29, 2024',
+  birthdate: 'Aug 05, 2024',
   name: 'Nurul'
 };
 
